ff13518e3b9b485fbe93ff4c136810db64154b6e
HyunMok-Chung <1021hm@naver.com>
2023-05-22 16:07:55 +0900

    feat: week6 day1  - 데이터베이스 연결 메소드 작성

---
+++ src/main/java/com/example/springbootdockerexercise/db/ConnectionChecker.java
@@ -0,0 +1,77 @@
+package com.example.springbootdockerexercise.db;
+
+import java.sql.*;
+import java.util.Map;
+
+import static java.lang.System.getenv;
+
+public class ConnectionChecker {
+    public void check() throws ClassNotFoundException, SQLException {
+        // DB 접속 코드
+        Map<String, String> env = getenv();
+        String dbHost = env.get("DB_HOST");
+        String dbUser = env.get("DB_USER");
+        String dbPassword = env.get("DB_PASSWORD");
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection con = DriverManager.getConnection(
+                dbHost,dbUser,dbPassword
+        );
+
+        Statement st = con.createStatement();
+        ResultSet rs = st.executeQuery("SHOW DATABASES");
+        rs = st.getResultSet();
+
+        while (rs.next()) {
+            String str = rs.getString(1);
+            System.out.println(str);
+        }
+    }
+    public void add() throws ClassNotFoundException, SQLException {
+
+        Map<String, String> env = getenv();
+        String dbHost = env.get("DB_HOST");
+        String dbUser = env.get("DB_USER");
+        String dbPassword = env.get("DB_PASSWORD");
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection con = DriverManager.getConnection(
+                dbHost,dbUser,dbPassword
+        );
+
+        PreparedStatement pstmt = con.prepareStatement("insert into users(id, name, password) values(?, ?, ?)");
+        pstmt.setString(1, "2");
+        pstmt.setString(2, "hyunmok2");
+        pstmt.setString(3, "1234");
+        pstmt.executeUpdate();
+    }
+    public void select() throws ClassNotFoundException, SQLException {
+        Map<String, String> env = getenv();
+        String dbHost = env.get("DB_HOST");
+        String dbUser = env.get("DB_USER");
+        String dbPassword = env.get("DB_PASSWORD");
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection con = DriverManager.getConnection(
+                dbHost,dbUser,dbPassword
+        );
+
+        Statement st = con.createStatement();
+        ResultSet rs = st.executeQuery("SELECT * FROM users LIMIT 50");
+        rs = st.getResultSet();
+
+        while(rs.next()){
+            String col1 =rs.getString(1);
+            String col2 =rs.getString(2);
+            String col3 =rs.getString(3);
+            System.out.printf("%s %s %s\n", col1, col2, col3);
+        }
+
+    }
+
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        ConnectionChecker cc = new ConnectionChecker();
+        Map<String, String> env = getenv();
+        System.out.println(env.get("DB_PASSWORD"));
+//        cc.check();
+//        cc.add();
+        cc.select();
+    }
+}
